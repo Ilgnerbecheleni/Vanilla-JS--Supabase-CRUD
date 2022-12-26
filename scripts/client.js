3b1bd77b29a4e2bd6aa2661ee337c59512b802d7
Ilgner Aparecido Becheleni <81272773+Ilgnerbecheleni@users.noreply.github.com>
2022-12-25 23:58:34 -0300

    Update client.js

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = "https://oxnrxhkiaqsewgjxgtkg.supabase.co"
+const SUPABASE_URL = "SUA_URL"
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94bnJ4aGtpYXFzZXdnanhndGtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE1Nzc5NTEsImV4cCI6MTk4NzE1Mzk1MX0.v6lIDHvru-PYXuRKDgpxtZQkdQdqIEVm7TlaPfI3rrM"
+const SUPABASE_ANON_KEY = "SUA_API_KEY"
 
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
